b13cff14dbb3f0482593dcc56ff6874706121f16
ReemAlattas <ralattas@live.com>
2017-08-07 15:50:27 -0400

    find second

---
+++ find_second.py
@@ -0,0 +1,20 @@
+# Define a procedure, find_second, that takes
+# two strings as its inputs: a search string
+# and a target string. It should return a
+# number that is the position of the second
+# occurrence of the target string in the
+# search string.
+
+
+def find_second(search_str, target_str):
+    first_index = search_str.find(target_str)
+    second_index = search_str.find(target_str, first_index+1)
+    return second_index
+
+danton = "De l'audace, encore de l'audace, toujours de l'audace"
+print find_second(danton, 'audace')
+#>>> 25
+
+twister = "she sells seashells by the seashore"
+print find_second(twister,'she')
+#>>> 13
