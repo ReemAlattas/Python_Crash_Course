762aa6e3c5e7aa810dcd851072defbc51134628e
ReemAlattas <ralattas@live.com>
2017-08-09 14:36:37 -0400

    get_all_links

--- stamps.py
+++ stamps.py
@@ -44,4 +44,24 @@ print stamps(5)
 print stamps(29)
 #>>> (5, 2, 0)  # five 5p stamps, two 2p stamps and no 1p stamps
 print stamps(0)
-#>>> (0, 0, 0) # no 5p stamps, no 2p stamps and no 1p stamps
+#>>> (0, 0, 0) # no 5p stamps, no 2p stamps and no 1p stamps# Define a procedure, find_element,
+# using index that takes as its
+# inputs a list and a value of any
+# type, and returns the index of
+# the first element in the input
+# list that matches the value.
+
+# If there is no matching element,
+# return -1.
+
+def find_element(p, val):
+    if val not in p:
+        return -1
+    else:
+        return p.index(val)
+
+print find_element([1,2,3],3)
+#>>> 2
+
+print find_element(['alpha','beta'],'gamma')
+#>>> -1
